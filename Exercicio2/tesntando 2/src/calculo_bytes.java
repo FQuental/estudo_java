4bcd6a04e6b7d78b6b610cbbb88c4872f17069e0
FQuental <felipedoquental@gmail.com>
2023-01-17 15:03:04 -0300

    Operações_Bytes

---
+++ Exercicio2/tesntando 2/src/calculo_bytes.java
@@ -0,0 +1,17 @@
+import java.io.SyncFailedException;
+
+public class calculo_bytes {
+    public static void main(String[] args) throws Exception {
+        byte a = 12;
+        byte d = 84;
+        short b = 54;
+        //int c = 500;
+
+        System.out.println("Valor de byte: " + a);
+        d = (byte) (d + a);
+        System.out.println("Valor de a+d: " + d);
+        b += d;
+        System.out.println("Valor de b+d+a: " + b);
+        a += d;
+    }
+}
