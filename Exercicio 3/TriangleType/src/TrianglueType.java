43e66b0439a4c0c083952396b46adae0b8d5ef7c
FQuental <felipedoquental@gmail.com>
2023-01-17 23:40:22 -0300

    TriangleType

---
+++ Exercicio 3/TriangleType/src/TrianglueType.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TrianglueType {
+    public static void main(String[] args) throws Exception {
+            Scanner sc = new Scanner(System.in);
+                System.out.println("Digite o valor do 1º lado: ");
+                int lado1=sc.nextInt();
+                System.out.println("Digite o valor do 2º lado: ");
+                int lado2=sc.nextInt();
+                System.out.println("Digite o valor do 3º lado: ");
+                int lado3=sc.nextInt();
+
+       if (lado1 == lado2 && lado2 == lado3 && lado1==lado3){
+            System.out.println("É um triangulo Equilatero.");
+        }if(lado1!=lado2 && lado1!=lado3 && lado2!=lado3){
+            System.out.println("É um triangulo Ecaleno.");
+        }if((lado1==lado2&&lado1!=lado3)||(lado2==lado3&&lado2!=lado1)||(lado1==lado3&&lado1!=lado2)){
+            System.out.println("É um triangulo Isocele.");
+        }
+
+
+    }
+}
